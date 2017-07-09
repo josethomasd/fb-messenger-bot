82724dadd617e209f2a4c35ee57faebf7c5bbd62
josethomasd <josethomasd@gmail.com>
2017-07-09 22:34:24 +0530

    runtie

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def send_message(recipient_id, message_text):
     if r.status_code != 200:
         log(r.status_code)
         log(r.text)
-
+    return "ok", 200
 
 def log(message):  # simple wrapper for logging to stdout on heroku
     print str(message)
